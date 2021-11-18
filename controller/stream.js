1e04886de93680c4b3c637a9eae250d33cef19fd
Pedro Ramos <pedro.ramos@betrybe.com>
2021-11-18 11:28:18 -0300

    REF: Remove comments

--- controller/stream.js
+++ controller/stream.js
@@ -2,15 +2,12 @@ const fs = require('fs');
 const service = require('../service/stream');
 
 module.exports = async (req, res) => {
-  // ensure that the request has the range
   const range = req.headers.range;
   if (!range) {
     res.status(400).send('Requires range header');
   }
 
-  // parse range
   const stats = await service.getStats(range);
-
   const headers = {
     'Content-Range': `bytes ${stats.start}-${stats.end}/${stats.size}`,
     'Accept-Encoding': 'bytes',
@@ -18,10 +15,8 @@ module.exports = async (req, res) => {
     'Content-Type': 'video/mp4'
   }
 
-  // 206 - partial content
   res.writeHead(206, headers);
 
-  // create stream for this chunk and send it to the client
   const interval = service.nextInterval(stats.end, stats.start);
 
   interval.pipe(res);
